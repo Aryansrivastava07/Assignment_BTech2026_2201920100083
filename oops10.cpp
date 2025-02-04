1470261e1ac05dde430f07de3f3fbb8a8e40037e
Aryan Srivastava <135363545+Aryansrivastava07@users.noreply.github.com>
2025-02-04 23:58:52 +0530

    Add files via upload

---
+++ oops10.cpp
@@ -0,0 +1,27 @@
+// Create a base class Vehicle with a protected member speed.
+//  Derive a class Car that inherits from Vehicle. Add a member function in Car to set the speed and another to display it.
+#include <iostream>
+
+class Vehicle {
+protected:
+    double speed;
+};
+
+class Car : public Vehicle {
+public:
+    void setSpeed(double s) {
+        speed = s;
+    }
+
+    void displaySpeed() const {
+        std::cout << "Speed of the car: " << speed << " km/h" << std::endl;
+    }
+};
+
+int main() {
+    Car myCar;
+    myCar.setSpeed(120.0);
+    myCar.displaySpeed();
+
+    return 0;
+}
